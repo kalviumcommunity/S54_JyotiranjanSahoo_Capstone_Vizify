10a5d83d4fdf9f90ed044c84d6412945bb06d3a7
unknown <sp577152@gmail.com>
2024-03-15 15:24:26 +0530

    Project Initialized

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require('cors')
+require('dotenv').config()
+
+
+const port = process.env.PORT || 5001
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.route('/').get((req,res)=>{
+    res.send("Hello")
+})
+
+
+app.listen(port,()=>{
+    console.log(`App is running on port ${port}`);
+})
